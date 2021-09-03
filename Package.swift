5d240da161cb16635132c586a4e933e04373ccf0
Steffan Andrews <stef@orchetect.com>
2021-09-02 23:48:51 -0700

    Updated MIDIKit dependency version to 0.1.18

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(url: "https://github.com/orchetect/MIDIKit", .branch("dev")), // , from: "0.1.16"),
+        .package(url: "https://github.com/orchetect/MIDIKit", from: "0.1.18"),
         .package(url: "https://github.com/orchetect/SwiftRadix", from: "1.0.3"),
     ],
 
