140b014882244125b6bf9d6903063308de1fd9d8
Steffan Andrews <stef@orchetect.com>
2021-12-05 17:12:34 -0800

    Bumped MIDIKit to 0.2.6

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(url: "https://github.com/orchetect/MIDIKit", .branch("main")), //, from: "0.2.5"),
+        .package(url: "https://github.com/orchetect/MIDIKit", from: "0.2.6"),
         .package(url: "https://github.com/orchetect/SwiftRadix", from: "1.0.3"),
     ],
 
